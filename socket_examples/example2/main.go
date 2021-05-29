6a1e6c99d9a7b6f99545111455709b14e764f4d0
Nhan Nguyen <nhanpublic@gmail.com>
2021-05-30 05:48:27 +0700

    refactor socket example 2

--- socket_examples/example2/main.go
+++ socket_examples/example2/main.go
@@ -21,85 +21,24 @@ import (
 )
 
 func main() {
-	if err := godotenv.Load("../../.env"); err != nil {
-		log.Fatalln("Error loading .env file: ", err)
-	}
-
-	db, err := gorm.Open(mysql.Open(os.Getenv("DSN")), &gorm.Config{})
-	if err != nil {
-		log.Fatalln("Error connecting database: ", err)
-	}
-
+	godotenv.Load("../../.env")
+	db, _ := gorm.Open(mysql.Open(os.Getenv("DSN")), nil)
 	appCtx := component.NewAppContext(db, nil, "", nil)
 	router := gin.New()
 	socketServer := socketio.NewServer(&engineio.Options{
 		Transports: []transport.Transport{websocket.Default},
 	})
 
-	socketServer.OnConnect("/", func(s socketio.Conn) error {
-		//s.SetContext("")
-		log.Println("connected:", s.ID(), " IP:", s.RemoteAddr())
-
-		//s.Join("Shipper")
-		//socketServer.BroadcastToRoom("/", "Shipper", "test", "Hello 200lab")
-
+	socketServer.OnConnect("/", func(socketConnection socketio.Conn) error {
+		//socketConnection.SetContext("")
+		log.Println("connected:", socketConnection.ID(), " IP:", socketConnection.RemoteAddr())
+		//socketConnection.Join("Shipper")
+		// socketServer.BroadcastToRoom("/", "Shipper", "test", "Hello 200lab")
 		return nil
 	})
 
-	socketServer.OnError("/", func(s socketio.Conn, e error) {
-		log.Println("meet error:", e)
-	})
-
-	socketServer.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		log.Println("closed", reason)
-		// Remove socket from socket router (from app context)
-	})
-
-	socketServer.OnEvent("/", "authenticate", func(s socketio.Conn, token string) {
-
-		// Validate token
-		// If false: s.Close(), and return
-
-		// If true
-		// => UserId
-		// Fetch db find user by Id
-		// Here: s belongs to who? (user_id)
-		// We need a map[user_id][]socketio.Conn
-
-		db := appCtx.GetMainDBConnection()
-		store := userstorage.NewSQLStore(db)
-		//
-		tokenProvider := jwt.NewTokenJWTProvider(appCtx.SecretKey())
-		//
-		payload, err := tokenProvider.Validate(token)
-
-		if err != nil {
-			s.Emit("authentication_failed", err.Error())
-			s.Close()
-			return
-		}
-		//
-		user, err := store.FindUser(context.Background(), map[string]interface{}{"id": payload.UserId})
-		//
-		if err != nil {
-			s.Emit("authentication_failed", err.Error())
-			s.Close()
-			return
-		}
-
-		if user.DeletedAt != nil {
-			s.Emit("authentication_failed", errors.New("you has been banned/deleted"))
-			s.Close()
-			return
-		}
-
-		user.Mask(false)
-
-		s.Emit("your_profile", user)
-	})
-
-	socketServer.OnEvent("/", "test", func(s socketio.Conn, msg string) {
-		log.Println(msg)
+	socketServer.OnEvent("/", "greeting", func(socketConnection socketio.Conn, msg string) {
+		log.Println("greeting from client:", msg)
 	})
 
 	type Person struct {
@@ -107,21 +46,59 @@ func main() {
 		Age  int    `json:"age"`
 	}
 
-	socketServer.OnEvent("/", "notice", func(s socketio.Conn, p Person) {
-		log.Println("socketServer receive notice:", p.Name, p.Age)
-
+	socketServer.OnEvent("/", "sendInfo", func(socketConnection socketio.Conn, p Person) {
+		log.Println("info sent from client:", p.Name, p.Age)
 		p.Age = 33
-		s.Emit("notice", p)
-
+		socketConnection.Emit("sendInfo", p)
 	})
 
-	socketServer.OnEvent("/", "test", func(s socketio.Conn, msg string) {
-		log.Println("socketServer receive test:", msg)
+	socketServer.OnEvent("/", "authenticate", func(socketConnection socketio.Conn, token string) {
+		// Validate token
+		// If false: socketConnection.Close(), and return
+		// If true
+		// => UserId
+		// Fetch db find user by Id
+		// Here: socketConnection belongs to who? (user_id)
+		// We need a map[user_id][]socketio.Conn
+
+		db := appCtx.GetMainDBConnection()
+		store := userstorage.NewSQLStore(db)
+		tokenProvider := jwt.NewTokenJWTProvider(appCtx.SecretKey())
+		payload, err := tokenProvider.Validate(token)
+		if err != nil {
+			socketConnection.Emit("authentication_failed", err.Error())
+			socketConnection.Close()
+			return
+		}
+
+		user, err := store.FindUser(context.Background(), map[string]interface{}{"id": payload.UserId})
+		if err != nil {
+			socketConnection.Emit("authentication_failed", "database error: " + err.Error())
+			socketConnection.Close()
+			return
+		}
+		if user.DeletedAt != nil {
+			socketConnection.Emit("authentication_failed", errors.New("you has been banned/deleted"))
+			socketConnection.Close()
+			return
+		}
+		user.Mask(false)
+
+		socketConnection.Emit("your_profile", user)
+	})
+
+	socketServer.OnError("/", func(socketConnection socketio.Conn, e error) {
+		log.Println("meet error:", e)
+	})
+
+	socketServer.OnDisconnect("/", func(socketConnection socketio.Conn, reason string) {
+		log.Println("closed", reason)
+		// Remove socket from socket router (from app context)
 	})
 
 	go func() {
 		if err := socketServer.Serve(); err != nil {
-			log.Fatalf("socketio listen error: %s\n", err)
+			log.Fatalf("socketio listen error: %socketConnection\n", err)
 		}
 	}()
 	defer socketServer.Close()
@@ -129,7 +106,6 @@ func main() {
 	router.GET("/socket.io/*any", gin.WrapH(socketServer))
 	router.POST("/socket.io/*any", gin.WrapH(socketServer))
 	router.StaticFS("/socket-example-2", http.Dir("./asset"))
-
 	if err := router.Run(":8000"); err != nil {
 		log.Fatal("failed run app: ", err)
 	}
