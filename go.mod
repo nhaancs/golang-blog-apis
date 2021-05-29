6624d73beba4eacab8ec20a5ab8ec90e35ddbc88
Nhan Nguyen <nhanpublic@gmail.com>
2021-05-29 22:34:42 +0700

    socket example

--- go.mod
+++ go.mod
@@ -8,8 +8,11 @@ require (
 	github.com/btcsuite/btcutil v1.0.2 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-gonic/gin v1.7.1
+	github.com/googollee/go-socket.io v1.6.0 // indirect
 	github.com/gosimple/slug v1.9.0 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	gorm.io/driver/mysql v1.0.5
 	gorm.io/gorm v1.21.6
---
+++ socket_example/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	socketio "github.com/googollee/go-socket.io"
+	"github.com/googollee/go-socket.io/engineio"
+	"github.com/googollee/go-socket.io/engineio/transport"
+	"github.com/googollee/go-socket.io/engineio/transport/websocket"
+)
+
+func main() {
+	router := gin.New()
+	socketServer := socketio.NewServer(&engineio.Options{
+		Transports: []transport.Transport{websocket.Default},
+	})
+
+	socketServer.OnConnect("/", func(socketConnection socketio.Conn) error {
+		socketConnection.SetContext("")
+		log.Println("connected: ", socketConnection.ID(), "ip: ", socketConnection.RemoteAddr())
+		return nil
+	})
+
+	// listen to msg event of /chat namespace
+	// process the message and return the data
+	socketServer.OnEvent("/chat", "msg", func(socketConnection socketio.Conn, msg string) string {
+		socketConnection.SetContext(msg)
+		return "received " + msg
+	})
+
+	// listen to notice event of root namespace
+	// process message and emit data to reply event of root namespace
+	socketServer.OnEvent("/", "notice", func(socketConnection socketio.Conn, msg string) {
+		log.Println("notice:", msg)
+		socketConnection.Emit("reply", "have "+msg)
+	})
+
+	socketServer.OnEvent("/", "bye", func(socketConnection socketio.Conn) string {
+		last := socketConnection.Context().(string)
+		socketConnection.Emit("bye", last)
+		socketConnection.Close()
+		return last
+	})
+
+	socketServer.OnError("/", func(socketConnection socketio.Conn, e error) {
+		log.Println("meet error:", e)
+	})
+
+	socketServer.OnDisconnect("/", func(socketConnection socketio.Conn, reason string) {
+		log.Println("closed", reason)
+	})
+
+	go func() {
+		if err := socketServer.Serve(); err != nil {
+			log.Fatalf("socketio listen error: %s\n", err)
+		}
+	}()
+	defer socketServer.Close()
+
+	router.GET("/socket.io/*any", gin.WrapH(socketServer))
+	router.POST("/socket.io/*any", gin.WrapH(socketServer))
+	router.StaticFS("/public", http.Dir("./asset"))
+
+	if err := router.Run(":8000"); err != nil {
+		log.Fatal("failed run app: ", err)
+	}
+}
